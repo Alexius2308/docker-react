d625e5d3b1057a6ca1f9bc8d6e3a13f41d0a0767
Your Name <you@example.com>
2021-07-05 10:31:00 +0300

    Changed app text

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Nothing to do here. It's better to follow this link.
         </a>
       </header>
     </div>
